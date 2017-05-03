f3538af5db91a1a59a47e4fc87ad0fa859f9a648
Peter Downs <peterldowns@gmail.com>
2017-05-03 18:51:17 -0400

    Add ingestion code to create database models from Met art.

--- art/models.py
+++ art/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
 # Create your models here.
+# What a row looks like from the database:
+{'Artist Alpha Sort': '',
+ 'Artist Begin Date': '',
+ 'Artist Display Bio': '',
+ 'Artist Display Name': '',
+ 'Artist End Date': '',
+ 'Artist Nationality': '',
+ 'Artist Prefix': '',
+ 'Artist Role': '',
+ 'Artist Suffix': '',
+ 'City': '',
+ 'Classification': 'Drawings',
+ 'Country': 'United States',
+ 'County': '',
+ 'Credit Line': 'Gift of Mrs. Robert W. de Forest, 1933',
+ 'Culture': 'American',
+ 'Department': 'American Decorative Arts',
+ 'Dimensions': '7 1/2 x 13 1/4 in. (19.1 x 33.7 cm)',
+ 'Dynasty': '',
+ 'Excavation': '',
+ 'Geography Type': 'Made in',
+ 'Is Highlight': 'False',
+ 'Is Public Domain': 'True',
+ 'Link Resource': 'http://www.metmuseum.org/art/collection/search/4939',
+ 'Locale': '',
+ 'Locus': '',
+ 'Medium': 'Ink and watercolor on paper',
+ 'Metadata Date': '5/1/2017 8:00:22 AM',
+ 'Object Begin Date': '1797',
+ 'Object Date': 'ca. 1800',
+ 'Object End Date': '1800',
+ 'Object ID': '4939',
+ 'Object Name': 'Manuscript sampler, Fraktur',
+ 'Period': '',
+ 'Portfolio': '',
+ 'Region': '',
+ 'Reign': '',
+ 'Repository': 'Metropolitan Museum of Art, New York, NY',
+ 'Rights and Reproduction': '',
+ 'River': '',
+ 'State': '',
+ 'Subregion': '',
+ 'Title': 'Manuscript Sampler',
+ '\ufeffObject Number': '34.100.72'}
+
+class Artist(models.Model):
+    name = models.TextField()
+    year_begin = models.IntegerField()
+    year_end = models.IntegerField()
+    bio = models.TextField()
+    nationality = models.TextField()
+
+class Artwork(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.TextField(blank=True, null=False)
+    classification = models.TextField(blank=True, null=False)
+    department = models.TextField(blank=True, null=False)
+    culture = models.TextField(blank=True, null=False)
+    medium = models.TextField(blank=True, null=False)
+
+    created = models.DateField(null=True)
+    museum_id = models.URLField(null=True)
+    museum_link = models.URLField(blank=True, null=False)
+    museum_name = models.TextField(blank=True, null=False)
+
+    public_domain = models.BooleanField()
+
--- data/inject.py
+++ data/inject.py
@@ -5,3 +5,51 @@ sys.path.append('..') # Root of django app
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
+from csv import DictReader
+from art.models import Artwork
+from art.models import Artist
+
+def load_full_collection():
+    with open('./MetObjects.csv') as fin:
+        for row in DictReader(fin):
+            yield row
+
+def cc_only(data):
+    for row in data:
+        if row['Is Public Domain'] == 'True':
+            yield row
+
+
+def create_object(row):
+    # TODO: create Artist object.
+    a = Artwork()
+    a.title = row['Title']
+    a.classification = row['Classification']
+    a.department = row['Department']
+    a.culture = row['Culture']
+    a.medium = row['Medium']
+
+    # TODO: better datetime parsing
+    try:
+        a.created = maya.parse(row['Object Begin Date']).datetime()
+    except Exception:
+        pass
+
+    a.museum_id = row['Object ID']
+    a.museum_link = row['Link Resource']
+    a.museum_name = 'Metropolitan Museum of Art'
+    a.public_domain = True
+
+    a.save()
+
+
+if __name__ == '__main__':
+    print('loading...')
+    cc = list(cc_only(load_full_collection()))
+    count = len(cc)
+    for i, row in enumerate(cc):
+        create_object(row)
+        if i % 50:
+            sys.stdout.write('\r%d/%d' % (i, len(cc)))
+    print('\nDone.')
+
