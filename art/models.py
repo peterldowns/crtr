6861b37ae7c178c53a46d95996122c5b5724e6a4
Peter Downs <peterldowns@gmail.com>
2017-05-13 18:44:50 -0400

    Set up a User and Collection model.

--- art/models.py
+++ art/models.py
@@ -1,50 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-# What a row looks like from the database:
-{'Artist Alpha Sort': '',
- 'Artist Begin Date': '',
- 'Artist Display Bio': '',
- 'Artist Display Name': '',
- 'Artist End Date': '',
- 'Artist Nationality': '',
- 'Artist Prefix': '',
- 'Artist Role': '',
- 'Artist Suffix': '',
- 'City': '',
- 'Classification': 'Drawings',
- 'Country': 'United States',
- 'County': '',
- 'Credit Line': 'Gift of Mrs. Robert W. de Forest, 1933',
- 'Culture': 'American',
- 'Department': 'American Decorative Arts',
- 'Dimensions': '7 1/2 x 13 1/4 in. (19.1 x 33.7 cm)',
- 'Dynasty': '',
- 'Excavation': '',
- 'Geography Type': 'Made in',
- 'Is Highlight': 'False',
- 'Is Public Domain': 'True',
- 'Link Resource': 'http://www.metmuseum.org/art/collection/search/4939',
- 'Locale': '',
- 'Locus': '',
- 'Medium': 'Ink and watercolor on paper',
- 'Metadata Date': '5/1/2017 8:00:22 AM',
- 'Object Begin Date': '1797',
- 'Object Date': 'ca. 1800',
- 'Object End Date': '1800',
- 'Object ID': '4939',
- 'Object Name': 'Manuscript sampler, Fraktur',
- 'Period': '',
- 'Portfolio': '',
- 'Region': '',
- 'Reign': '',
- 'Repository': 'Metropolitan Museum of Art, New York, NY',
- 'Rights and Reproduction': '',
- 'River': '',
- 'State': '',
- 'Subregion': '',
- 'Title': 'Manuscript Sampler',
- '\ufeffObject Number': '34.100.72'}
+
+class User(AbstractUser):
+    pass
 
 
 class Artist(models.Model):
@@ -81,3 +40,13 @@ class Artwork(models.Model):
 
     image_url_small = models.URLField(null=True)
     image_url_large = models.URLField(null=True)
+
+
+class Collection(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.TextField(blank=True, null=False)
+    user = models.ForeignKey('art.User', related_name='collections')
+    artworks = models.ManyToManyField('art.Artwork',
+                                      related_name='collections')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
--- settings.py
+++ settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'art',
 ]
 
+AUTH_USER_MODEL = 'art.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
